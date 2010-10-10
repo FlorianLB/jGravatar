04f4258e5ac30f5928b01e83570c7f45374057e3
FlorianLB <f.lonqueu.brochard+github@gmail.com>
2010-10-10 23:57:49 +0200

    PHP Doc

--- plugin/tpl/html/function.jgravatar.php
+++ plugin/tpl/html/function.jgravatar.php
@@ -1,12 +1,10 @@
 <?php
 /**
-* @package    jelix
+* @package    jGravatar
 * @subpackage jtpl_plugin
-* @version    $Id$
-* @author     Jouanneau Laurent
-* @copyright  2005-2006 Jouanneau laurent
-* @link        http://www.jelix.org
-* @licence    GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
+* @author     Florian Lonqueu-Brochard
+* @copyright  2010 Florian Lonqueu-Brochard
+* @licence    MIT
 */
 
 
@@ -15,10 +13,7 @@ jClasses::inc('jGravatar~jGravatar');
 /**
  * function plugin :  write the image tag that display the gravatar
  *
- * @param jTpl $tpl template engine
- * @param string $selector selector action
- * @param array $params parameters for the url
- * @param boolean $escape if true, then escape the string for html
+ * @see jGravatar::get
  */
 function jtpl_function_html_jgravatar($tpl, $email, $gravatar_size = null, $rating = null , $alt ='')
 {
